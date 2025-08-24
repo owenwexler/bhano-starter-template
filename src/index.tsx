d9ff1d2f7ebc76ea3740e0ff76e197efc8ee339c
Owen Wexler <owenbwexler@gmail.com>
2025-08-24 11:28:18 -0400

    Implement @hono/bun-compress to GZIP responses for slower internet connections

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,17 @@ import Body from './components/Body/Body';
 import apiRoute from './routes/apiRoute';
 import Loading from './components/Loading/Loading';
 
+// this is a polyfill for hono/compress meant to work with Bun until CompressionStream is implemented natively in Bun
+// used to GZIP static files and HTML/HTMX responses before sending over the wire
+// GZIPPING HTMX and Alpine reduces initial page load time by an average of 1.3-1.7 seconds on slower Internet connections
+import { compress } from '@hono/bun-compress';
+
 const app = new Hono();
 
-app.use('/static/*', serveStatic({ root: './' }))
+// implement the compression
+app.use(compress());
+
+app.use('/static/*', serveStatic({ root: './' }));
 
 app.route('/partials', partialsRoute);
 app.route('/api', apiRoute);
